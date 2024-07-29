2be9d172714368f94cf04a95490b160b73ecbe22
Ziad Ahmed <ziadwizard612@gmail.com>
2024-07-29 15:32:51 +0300

    Fixed bugs

--- UART_Library/example.c
+++ UART_Library/example.c
@@ -20,7 +20,7 @@ int main(void)
 	* 5- Setting Stop bit(s) to 1 bit
 	* 6- Setting Data bits to 8 bits
 	* 7- ! Clock Polarity is Synchronous mode only ! Write this bit to zero since Asynchronous is selected 
-	*							Rising_XCK_Edge = 0 , Falling+XCK_Edge = 1
+	*							Rising_XCK_Edge = 0 , Falling_XCK_Edge = 1
 	*/
 	
 	uart_init(BAUD_9600,FALSE, DISABLE_PARITY,Asynchronous,ONE_BIT,EIGHT_BIT,Rising_XCK_Edge);
